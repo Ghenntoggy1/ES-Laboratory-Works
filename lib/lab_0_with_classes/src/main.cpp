2af794511fef55d472c41686921bc60600466a5c
Ghenntoggy1 <roman.gusev@isa.utm.md>
2025-02-16 00:46:06 +0200

    fix: Fixed imports in main.cpp for Laboratory Work 0

--- lib/lab_0_with_classes/src/main.cpp
+++ lib/lab_0_with_classes/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include "main.h"
-#include "src/dd_led/dd_led.h"
-#include "src/dd_button/dd_button.h"
+#include "dd_led/dd_led.h"
+#include "dd_button/dd_button.h"
 
 Led led(13);
 Button button(12);
