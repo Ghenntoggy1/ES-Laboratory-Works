ffc96453db126bebd63bb1aec481f0219a81c725
Ghenntoggy1 <roman.gusev@isa.utm.md>
2025-02-16 03:11:22 +0200

    feat: Add Laboratory Work 1.1 - LED guided by commands. refactor: Files Renamed.

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,20 @@
 #include "lab_0_main.h"
 #include "lab_1_1_main.h"
 
-#define APP_NAME LAB_1_1
+#define APP_NAME "LAB_1_1"
 
 void setup() {
-    #if APP_NAME == LAB_0
+    if (APP_NAME == "LAB_0") {
         lab_0_setup();
-    #elif APP_NAME == LAB_1_1
+    } else if (APP_NAME == "LAB_1_1") {
         lab_1_1_setup();
-    #endif
+    }
 }
 
 void loop() {
-    #if APP_NAME == LAB_0
+    if (APP_NAME == "LAB_0") {
         lab_0_loop();
-    #elif APP_NAME == LAB_1_1
+    } else if (APP_NAME == "LAB_1_1") {
         lab_1_1_loop();
-    #endif
+    }
 }
