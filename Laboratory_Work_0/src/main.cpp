9dd9dca6e73d1fee70c076768c3f35a62328b87d
Ghenntoggy1 <roman.gusev@isa.utm.md>
2025-03-16 16:14:01 +0200

    Restructure Project

---
+++ Laboratory_Work_0/src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+#include "lab_0/src/lab_0_main.h"
+
+void setup() {
+    lab_0_setup();
+}
+
+void loop() {
+    lab_0_loop();
+}
--- src/lab_2_2/src/lab_2_2_main.h
+++
@@ -1,7 +0,0 @@
-#ifndef LAB_2_2_MAIN_H
-#define LAB_2_2_MAIN_H
-
-void lab_2_2_setup();
-void lab_2_2_loop();
-
-#endif
