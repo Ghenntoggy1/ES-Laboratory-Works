4ad40e6463ec5564bfa9190a31c97e0e6aac1f03
Ghenntoggy1 <roman.gusev@isa.utm.md>
2025-03-31 17:30:43 +0300

    init Laboratory Work 4.1

---
+++ Laboratory_Work_4_1/src/lab_4_1/lab_4_1.cpp
@@ -0,0 +1,13 @@
+#include "lab_4_1.h"
+#include "srv_serial_stdio.h"
+
+void lab_4_1_setup() {
+    srvSerialSetup();
+    printf("Lab 4.1 setup done\n");
+
+}
+
+void lab_4_1_loop() {
+
+}
+
---
+++ Laboratory_Work_4_1/src/lab_4_1/lab_4_1.h
@@ -0,0 +1,7 @@
+#ifndef LAB_4_1_H
+#define LAB_4_1_H
+
+void lab_4_1_setup();
+void lab_4_1_loop();
+
+#endif
