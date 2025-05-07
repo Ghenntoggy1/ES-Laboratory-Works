750bb3a70c013c9948bfb35db51259602a38a386
Ghenntoggy1 <roman.gusev@isa.utm.md>
2025-05-07 23:16:32 +0300

    feat(lab_6_1): Add new Transition to the Table.

--- Laboratory_Work_6_1/lib/srv_fsm/srv_fsm.h
+++ Laboratory_Work_6_1/lib/srv_fsm/srv_fsm.h
@@ -4,6 +4,9 @@
 #include "dd_button.h"
 #include <Arduino.h>
 
+#define FSM_TRANSITION_COUNT 4
+#define FSM_STATE_COUNT 3
+
 typedef enum {
     STATE_OFF,
     STATE_BLINK,
@@ -11,23 +14,24 @@ typedef enum {
     STATE_COUNT,
 } FSMState;
 
-static const char* FSMStateStrings[] = {
+static const char* FSMStateStrings[FSM_STATE_COUNT] = {
     "STATE_OFF",
     "STATE_BLINK",
-    "STATE_ON",
-    "STATE_COUNT"
+    "STATE_ON"
 };
 
 typedef enum {
     OFF_TO_BLINKING,
     BLINKING_TO_ON,
-    ON_TO_OFF
+    ON_TO_BLINKING,
+    BLINKING_TO_OFF
 } FSMTransition;
 
-static const char* FSMTransitionStrings[] = {
+static const char* FSMTransitionStrings[FSM_TRANSITION_COUNT] = {
     "OFF_TO_BLINKING",
     "BLINKING_TO_ON",
-    "ON_TO_OFF"
+    "ON_TO_BLINKING",
+    "BLINKING_TO_OFF"
 };
 
 struct StateTable {
@@ -39,7 +43,7 @@ struct StateTable {
 };
 typedef const struct StateTable StateTableStruct;
 
-extern StateTableStruct stateTable[STATE_COUNT];
+extern StateTableStruct stateTable[FSM_TRANSITION_COUNT];
 
 void initFSM();
 void runFSM();
