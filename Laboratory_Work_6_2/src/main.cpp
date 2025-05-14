6b0cee3b0163c950bb54d4905ff6bf9abaeb9cac
Ghenntoggy1 <roman.gusev@isa.utm.md>
2025-05-15 01:46:19 +0300

    feat(lab_6_2): Add FSM Logic.

--- Laboratory_Work_6_2/src/main.cpp
+++ Laboratory_Work_6_2/src/main.cpp
@@ -1,18 +1,10 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include "lab_6_2/lab_6_2.h"
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    lab_6_2_setup();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    lab_6_2_loop();
 }
